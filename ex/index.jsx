3c7ce137f400e3b07da4cd54d1437282caec7069
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2018-01-09 12:37:56 -0200

    trabalhanado com valor controlado 2

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ClassComponent from './classComponent';
+import Field from './field';
 
 ReactDOM.render(
-    <ClassComponent initialValue={10} />
+    <Field initialValue={10} />
 , document.querySelector("#app"));
